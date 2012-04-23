942122d5e45fd26ff5c0af976f64f880d9e593dc
scottjehl <scott@scottjehl.com>
2012-04-23 20:50:55 +0530

    removed demo files, as they're now in the Southstreet repo

--- _demo/sample-files/generalenhancements.js
+++
@@ -1,6 +0,0 @@
-// NOTE: the real contents of this file would actually do something useful.
-// for demo purposes, it logs a message to the page.
-var p = document.createElement( "p" );
-p.className = "general";
-p.innerHTML = "generalenhancements.js was loaded because it is meant for all enhanced browsers";
-document.body.appendChild( p );
--- _demo/sample-files/touch.js
+++
@@ -1,5 +0,0 @@
-// NOTE: the real contents of this file would actually do something useful.
-// for demo purposes, it logs a message to the page.
-var p = document.createElement( "p" );
-p.innerHTML = "generalenhancements.js was loaded because touch events are supported";
-document.body.appendChild( p );
