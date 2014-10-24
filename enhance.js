27758acaa3822e46fa002894eaf406d1c9a9bdbd
Scott Jehl <scott@filamentgroup.com>
2014-10-24 14:28:19 -0500

    update to latest loadJS version

--- enhance.js
+++ enhance.js
@@ -27,6 +27,7 @@
 		var ref = window.document.getElementsByTagName( "script" )[ 0 ];
 		var script = window.document.createElement( "script" );
 		script.src = src;
+		script.async = true;
 		ref.parentNode.insertBefore( script, ref );
 		return script;
 	}
